c8002cff92f14678223e055427d6a897179b39e5
Rilton SM <rilton_moreira@hotmail.com>
2022-05-29 17:47:00 -0300

    Projeto finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const { user_id } = request.headers;
+            const id = String(user_id);
+
+            const list = this.listAllUsersUseCase.execute({ user_id: id });
+            return response.json(list);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
